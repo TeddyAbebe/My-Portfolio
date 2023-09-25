883f16c36a2d5b5c94cac80549a4f276193c95b0
Tewodros Abebe <teddyab729@gmail.com>
2023-09-25 08:51:23 +0300

    Update

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -52,7 +52,7 @@ const Frontend = () => {
           <div className="skills__data">
             <i class="bx bx-badge-check"></i>
             <div>
-              <h3 className="skills__name">Git</h3>
+              <h3 className="skills__name">Emotion</h3>
               <span className="skills__level">Intermediate</span>
             </div>
           </div>
